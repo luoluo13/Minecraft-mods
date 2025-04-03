2cd553f28ac8d2760f117850eb588aa9736278df
Nefeli <1084272329@qq.com>
2025-04-03 23:54:42 +0800

    0.1.0 增加四个附魔类、一个附魔效果实现类 实现了吸血（剑），霰弹（弓）、速射（弓），直射（弓）

---
+++ sourceCode/Forge1.20.1/Nefeli/src/main/java/top/nefeli/minecraft/ExampleMod.java
@@ -0,0 +1,23 @@
+package top.nefeli.minecraft;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.nefeli.minecraft.enchantment.EnchantmentHandler;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "examplemod";
+    // Directly reference a slf4j logger
+    public ExampleMod() {
+        MinecraftForge.EVENT_BUS.register(new EnchantmentHandler());
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // 注册附魔
+        ModEnchantments.ENCHANTMENTS.register(modEventBus);
+    }
+}
---
+++ sourceCode/Forge1.20.1/Nefeli/src/main/java/top/nefeli/minecraft/ModEnchantments.java
@@ -0,0 +1,31 @@
+package top.nefeli.minecraft;
+
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import top.nefeli.minecraft.enchantment.LifeStealEnchantment;
+import top.nefeli.minecraft.enchantment.RapidFireEnchantment;
+import top.nefeli.minecraft.enchantment.ScatteringFireEnchantment;
+import top.nefeli.minecraft.enchantment.StiffnessEnchantment;
+
+
+public class ModEnchantments {
+    // 1. 创建 DeferredRegister
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS =
+            DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, ExampleMod.MODID);
+
+    // 2. 注册附魔
+    public static final RegistryObject<Enchantment> STIFFNESS =
+            ENCHANTMENTS.register("stiffness", StiffnessEnchantment::new);
+    public static final RegistryObject<Enchantment> LIFE_STEAL =
+            ENCHANTMENTS.register("life_steal", LifeStealEnchantment::new);
+    public static final RegistryObject<Enchantment> RAPID_FIRE  =
+            ENCHANTMENTS.register("rapid_fire", RapidFireEnchantment::new);
+
+    public static final RegistryObject<Enchantment> SCATTERING_FIRE  =
+            ENCHANTMENTS.register("scattering_fire", ScatteringFireEnchantment::new);
+
+    // 3. 在 Mod 主类中调用注册
+    // 见下一步
+}
